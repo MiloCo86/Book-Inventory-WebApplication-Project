ff018110fee626bbf3b0a64e25f1c2b560e2efe6
MiloCo86 <cpaez@pursuit.org>
2024-03-21 11:37:08 -0400

    html and css almost completed

---
+++ Data/bookData.js
@@ -0,0 +1,19 @@
+const books = [
+    {
+        title: 'One Hundred Years of Solitude',
+        author: 'Gabriel Garcia Marquez',
+        url: 'https://m.media-amazon.com/images/I/51p+yiLxdfS._SY445_SX342_.jpg',
+        stock: 'in stock',
+        price: '$52.99'
+    },
+    {
+        title: 'The Da Vinci Code',
+        author: 'Dan Brown',
+        url: 'https://pictures.abebooks.com/isbn/9780385504201-us.jpg',
+        stock: 'out of stock',
+        price: '$45.99'
+    },
+    
+];
+
+export {books};
